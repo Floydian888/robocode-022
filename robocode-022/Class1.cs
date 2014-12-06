4e0cefece186c0a6aa28a62595e260f3edcbc443
Adam <adam.duluk@gmail.com>
2014-12-06 02:45:20 +0100

    MIdziemySpac

--- robocode-022/Class1.cs
+++ robocode-022/Class1.cs
@@ -3,45 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 using Robocode;
 
 namespace DLKWGD
 {
     class zero22 : AdvancedRobot
     {
-
-        
         double kierunek = 1;
         public void Move() {
-            
-            Ahead(5000);
+            SetAhead(5000);
+            WaitFor(new MoveCompleteCondition(this));
             TurnRight(90);
-            //TurnRadarRight(Heading + 90);
-            while (DistanceRemaining > 0 && TurnRemaining > 0)
-            {
-                Execute();
-            }
-            
-        }
-
-        public double ObliczKat(double x, double y)
-        {
-            double kat;
-            return kat = (Math.Atan(x / y)) * (180 / Math.PI);
+               
         }
 
         public void skanujOscylacyjnie(double kierunek)
         {
-            SetTurnRadarRight( 50 * kierunek);
-           // SetTurnGunRight(50 * kierunek);
+            SetTurnRadarRight( 70 * kierunek);
             kierunek *= -1;
-            SetTurnRadarRight( 100 * kierunek);
-            //SetTurnGunRight(100 * kierunek);
+            SetTurnRadarRight( 140 * kierunek);
         }
 
         public override void Run()
         {
-
+            
             IsAdjustGunForRobotTurn = true;
             IsAdjustRadarForRobotTurn = true;
             TurnRight(360 - Heading);
@@ -49,43 +35,37 @@ namespace DLKWGD
             TurnRight(90);
             Ahead(5000);
             TurnRight(90);
-            //TurnRadarRight(90);
 
-            //double kat = ObliczKat(BattleFieldWidth, BattleFieldHeight);
             while (true)
             {
                 Move();
-                
             }
-
         }
-
+        public void Scanner() {
+            SetTurnRadarRight(360 - RadarHeading);
+            SetTurnRadarRight(Heading + 90);
+        
+        }
         public override void OnHitRobot(HitRobotEvent evnt)
         {
-
-            Reset();
-            Move();
-        }
-        public void Reset()
-        {
-            TurnRight(360 - Heading);
-            Ahead(5000);
-            TurnRight(90);
-            Ahead(5000);
+            SetAhead(5000);
+            WaitFor(new MoveCompleteCondition(this));
+            TurnRight(90); 
         }
         
         public override void OnScannedRobot(ScannedRobotEvent evnt)
         {
             //IsInterruptible = true;
             SetTurnGunRight(Heading - GunHeading + evnt.Bearing);
+            
             while(GunTurnRemaining > 0){
                 Execute();
             }
-            if (evnt.Distance < 100)
+            if (evnt.Distance < 100 && GunHeat < 1)
             {
                 SetFire(3);
             }
-            else
+            else if(GunHeat < 1)
             {
                 SetFire(1);
             }
