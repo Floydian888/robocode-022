1f2917b74f0cbd767e7252fd861723af8e2aaf0f
Adam <adam.duluk@gmail.com>
2014-12-06 01:12:15 +0100

    WorkInProgessScan

--- robocode-022/Class1.cs
+++ robocode-022/Class1.cs
@@ -9,40 +9,78 @@ namespace DLKWGD
 {
     class zero22 : AdvancedRobot
     {
-        double x_pole = 0, y_pole = 0;
+
+        
         double kierunek = 1;
-        public void okreslWymiaryPola()
+        public void Move() {
+            
+            Ahead(5000);
+            TurnRight(90);
+            //TurnRadarRight(Heading + 90);
+            while (DistanceRemaining > 0 && TurnRemaining > 0)
+            {
+                Execute();
+            }
+            
+        }
+
+        public double ObliczKat(double x, double y)
         {
-            TurnRight(360 - Heading);
-            SetAhead(5000);
-            SetTurnRight(90);
-            SetAhead(5000);
-            Console.WriteLine(X + ", " + Y);
-            x_pole = X;
-            y_pole = Y;
+            double kat;
+            return kat = (Math.Atan(x / y)) * (180 / Math.PI);
         }
 
         public void skanujOscylacyjnie(double kierunek)
         {
             SetTurnRadarRight( 50 * kierunek);
-            SetTurnGunRight(50 * kierunek);
+           // SetTurnGunRight(50 * kierunek);
             kierunek *= -1;
             SetTurnRadarRight( 100 * kierunek);
-            SetTurnGunRight(100 * kierunek);
+            //SetTurnGunRight(100 * kierunek);
         }
 
         public override void Run()
         {
-            okreslWymiaryPola();
-            Console.WriteLine(x_pole + " " + y_pole);
-            while (DistanceRemaining > 0 && TurnRemaining > 0 && RadarTurnRemaining> 0)
+
+            IsAdjustGunForRobotTurn = true;
+            IsAdjustRadarForRobotTurn = true;
+            TurnRight(360 - Heading);
+            Ahead(5000);
+            TurnRight(90);
+            Ahead(5000);
+            TurnRight(90);
+            //TurnRadarRight(90);
+
+            //double kat = ObliczKat(BattleFieldWidth, BattleFieldHeight);
+            while (true)
             {
-                Execute();
+                Move();
+                
             }
+
         }
 
+        public override void OnHitRobot(HitRobotEvent evnt)
+        {
+
+            Reset();
+            Move();
+        }
+        public void Reset()
+        {
+            TurnRight(360 - Heading);
+            Ahead(5000);
+            TurnRight(90);
+            Ahead(5000);
+        }
+        
         public override void OnScannedRobot(ScannedRobotEvent evnt)
         {
+            //IsInterruptible = true;
+            SetTurnGunRight(Heading - GunHeading + evnt.Bearing);
+            while(GunTurnRemaining > 0){
+                Execute();
+            }
             if (evnt.Distance < 100)
             {
                 SetFire(3);
@@ -51,7 +89,6 @@ namespace DLKWGD
             {
                 SetFire(1);
             }
-            IsAdjustRadarForRobotTurn = true;
             Console.WriteLine("wyskanowano, distance: " + evnt.Distance + ", gun heat: " + GunHeat);
             skanujOscylacyjnie(kierunek);
             kierunek *= -1;
